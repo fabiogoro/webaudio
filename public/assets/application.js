356a79d6099a087bb33bdae64165a438e8e9800a
Fábio Goródscy <fabiogorodscy@hotmail.com>
2016-03-19 12:09:12 -0300

    Connection test

--- public/assets/application.js
+++ public/assets/application.js
@@ -1,17 +1,27 @@
-var peer = new Peer({key: 'ofs1nu2rh3t0529'});
+$(document).ready(function() {
+  peer = new Peer({ key: 'lwjd5qra8257b9', debug: 3});
 
-peer.on('open', function(id) {
-  $("#my_id").html('My peer ID is: ' + id);
+  peer.on('open', function(id){
+    $('#my_id').html('My peer ID is: ' + id);
+
+    $('#connect_button').attr('disabled',false);
+  });
+
+  peer.on('connection', function(connection) {
+    connection.on('open', function() {
+      data = 'Hello, peer'
+      $('#helloworld').append(data);
+      connection.send(data);
+    });
+    connection.on('data', function(data) {
+      $('#helloworld').append(data);
+    });
+  });
 });
 
 function connect() {
-  var conn = peer.connect($("#dest_id").val());
-  conn.on('open', function() {
-    // Receive messages
-    conn.on('data', function(data) {
-      console.log('Received', data);
-    });
-
-    conn.send('Hello!');
+  var c = peer.connect($('#dest_id').val());
+  c.on('data', function(data) {
+    $('#helloworld').append(data);
   });
 }
