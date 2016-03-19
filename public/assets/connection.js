7c24efe1b3336bdbdd4e2327bc2833c71a1e0379
Fábio Goródscy <fabiogorodscy@hotmail.com>
2016-03-19 16:45:10 -0300

    End

--- public/assets/connection.js
+++ public/assets/connection.js
@@ -2,9 +2,9 @@ var peer;
 var connection;
 
 function create() {
-  my_id = Math.floor((Math.random() * 100000) + 1;
+  my_id = Math.floor((Math.random() * 100000) + 1);
   peer = new Peer(my_id, { key: 'ofs1nu2rh3t0529' });
-  $('#connection_status').html("Share your session id: " + my_id);
+  $('#dest_id').val('Share your session id: ' + my_id);
 
   peer.on('connection', function(c) {
     connection = c;
@@ -23,6 +23,7 @@ function create() {
 function connect() {
   peer = new Peer({ key: 'ofs1nu2rh3t0529' });
   connection = peer.connect($('#dest_id').val());
+  $('#dest_id').attr('disabled', true);
   connection.on('data', function(data) {
     $('#connection_status').html(data.name);
     key_lookup(data);
