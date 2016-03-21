23200c6f6aaf969bc8fd3e8fbf246767135d13c1
Fábio Goródscy <fabiogorodscy@hotmail.com>
2016-03-21 10:40:44 -0300

    8th example: pubnub

---
+++ public/8/assets/connection.js
@@ -0,0 +1,21 @@
+var pubnub = PUBNUB({
+    subscribe_key: 'sub-c-6f08dafe-ef69-11e5-872f-02ee2ddab7fe',
+    publish_key: 'pub-c-d499c43e-d44f-4daf-b7af-23414ef0019a'
+});
+
+pubnub.subscribe({
+  channel : "piano",
+  callback: function(data) {
+      key_lookup(data);
+  },
+  error: function(err) {
+      console.log(err);
+  }
+})
+
+function send(data) {
+  pubnub.publish({
+    channel: 'piano',
+    message: data
+  });
+}
